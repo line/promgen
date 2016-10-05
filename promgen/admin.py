13e459cad0511aa65a862eb8d824dc1e2ce62897
Paul Traylor <paul.traylor@linecorp.com>
2016-10-05 17:55:13 +0900

    Initial admin and model setup

--- promgen/admin.py
+++ promgen/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from promgen import models
 
-# Register your models here.
+admin.site.register(models.Service)
+admin.site.register(models.Host)
+
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'service', 'farm')
+
+
+@admin.register(models.Farm)
+class FarmAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(models.Exporter)
+class ExporterAdmin(admin.ModelAdmin):
+    list_display = ('job', 'port', 'path', 'project')
+    list_filter = ('project', 'job', 'port')
+
+
+@admin.register(models.Rule)
+class RuleAdmin(admin.ModelAdmin):
+    list_display = ('name', 'clause', 'duration', 'labels', 'annotations', 'service')
+    list_filter = ('service',)
--- promgen/models.py
+++ promgen/models.py
@@ -2,4 +2,55 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
+    farm = models.ForeignKey('Farm', blank=True, null=True)
+
+    def __str__(self):
+        return '{}:{}'.format(self.service.name, self.name)
+
+
+class Farm(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    source = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Host(models.Model):
+    name = models.CharField(max_length=128)
+    farm = models.ForeignKey('Farm', on_delete=models.CASCADE)
+
+    unique_together = ('name', 'farm')
+
+
+class Exporter(models.Model):
+    job = models.CharField(max_length=128)
+    port = models.IntegerField()
+    path = models.CharField(max_length=128)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+
+    unique_together = ('port', 'path', 'project')
+
+
+class Rule(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    clause = models.CharField(max_length=128)
+    duration = models.CharField(max_length=128, choices=[
+        ('1s', '1s'),
+        ('1m', '1m'),
+        ('5m', '5m'),
+    ])
+    labels = models.CharField(max_length=128)
+    annotations = models.CharField(max_length=128)
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
