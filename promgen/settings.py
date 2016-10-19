b1f998f59f6db478fffa4e5a19e27e49ab1bd993
Paul Traylor <paul.traylor@linecorp.com>
2016-10-20 08:50:47 +0900

    Add STATIC_ROOT for hosting

--- promgen/settings.py
+++ promgen/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.expanduser('~/.cache/promgen')
 
 if DEBUG:
     try:
