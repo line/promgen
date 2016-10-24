4da26b7fe1720f5780fd1407b108595d4d09f482
Paul Traylor <paul.traylor@linecorp.com>
2016-10-24 16:03:07 +0900

    Webhook sender

--- promgen/sender/webhook.py
+++ promgen/sender/webhook.py
@@ -1,19 +1,25 @@
 import logging
-
-from promgen.models import Project
+import requests
+from promgen.models import Sender
 
 logger = logging.getLogger(__name__)
 
 
 def send(data):
     for alert in data['alerts']:
-        for project in Project.objects.filter(name=alert['labels'].get('project')):
-            logger.debug('Sending %s for %s', __name__, project.name)
-            for sender in project.sender_set.filter(sender=__name__):
-                logger.info('%s to %s', alert['labels']['alertname'], sender.value)
-                break
-            else:
-                logger.debug('No senders configured for %s->%s', project,  __name__)
+        project = alert['labels'].get('project')
+        for sender in Sender.objects.filter(sender=__name__, project__name=project):
+            logger.debug('Sending %s for %s', __name__, project)
+
+            data = {
+                'prometheus': alert['generatorURL'],
+                'status': alert['status'],
+                'alertmanager': data['externalURL']
+            }
+            data.update(alert['labels'])
+            data.update(alert['annotations'])
+
+            requests.post(sender.value, data).raise_for_status()
             break
         else:
-            logger.debug('No senders configured for project', )
+            logger.debug('No senders configured for %s->%s', project,  __name__)
---
+++ promgen/tests/test_webhook.py
@@ -0,0 +1,41 @@
+import mock
+from django.test import TestCase, override_settings
+
+from promgen import models
+from promgen.sender.webhook import send
+from promgen.tests import TEST_ALERT, TEST_SETTINGS
+
+
+_PARAMS = {
+    'alertmanager': 'https://am.promehteus.localhost',
+    'alertname': 'node_down',
+    'description': 'testhost.localhost:9100 of job node has been down for more than 5 minutes.',
+    'env': 'prod',
+    'farm': 'foo-BETA',
+    'instance': 'testhost.localhost:9100',
+    'job': 'node',
+    'monitor': 'prometheus',
+    'project': 'Project 1',
+    'prometheus': 'https://monitoring.promehteus.localhost/graph#%5B%7B%22expr%22%3A%22up%20%3D%3D%200%22%2C%22tab%22%3A0%7D%5D',
+    'severity': 'critical',
+    'status': 'resolved',
+    'summary': 'Instance testhost.localhost:9100 down',
+}
+
+
+class IkasanTest(TestCase):
+    @mock.patch('django.db.models.signals.post_save', mock.Mock())
+    def setUp(self):
+        self.service = models.Service.objects.create(name='Service 1')
+        self.project = models.Project.objects.create(name='Project 1', service=self.service)
+        self.sender = models.Sender.objects.create(
+            project=self.project,
+            sender='promgen.sender.webhook',
+            value='http://example.com',
+        )
+
+    @override_settings(PROMGEN=TEST_SETTINGS)
+    @mock.patch('requests.post')
+    def test_project(self, mock_post):
+        send(TEST_ALERT)
+        mock_post.assert_called_once_with('http://example.com', _PARAMS)
