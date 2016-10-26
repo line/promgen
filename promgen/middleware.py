96ca17d8dd2a0cb7efdbbbd2230b25f95eebcd70
Paul Traylor <paul.traylor@linecorp.com>
2016-10-26 11:58:53 +0900

    Show messages when writing target config or rules

--- promgen/middleware.py
+++ promgen/middleware.py
@@ -6,6 +6,7 @@ files, we need to handle some deduplication. This is handled by using the django
 caching system to set a key and then triggering the actual event from middleware
 '''
 
+from django.contrib import messages
 from django.core.cache import cache
 
 from promgen.signals import write_config, write_rules
@@ -20,10 +21,12 @@ class RemoteTriggerMiddleware(object):
 
         if cache.get('write_config'):
             write_config.send(self)
+            messages.info(request, 'Wrote Config')
             cache.delete('write_config')
 
         if cache.get('write_rules'):
             write_rules.send(self)
+            messages.info(request, 'Wrote Rules')
             cache.delete('write_rules')
 
         return response
