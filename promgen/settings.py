d7fae50fef21fb65ee8ab6c78b7a4ac85dc8db2c
Paul Traylor <paul.traylor@linecorp.com>
2016-10-27 15:00:15 +0900

    Initial i18n work

--- promgen/settings.py
+++ promgen/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
