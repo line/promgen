665fa9320709e8d83cac591acd12d738fc6079d2
Paul Traylor <paul.traylor@linecorp.com>
2016-12-20 11:16:44 +0900

    Loop through annotations in notification body

--- promgen/tests/test_ikasan.py
+++ promgen/tests/test_ikasan.py
@@ -8,8 +8,8 @@ from promgen.tests import TEST_ALERT, TEST_SETTINGS
 
 _MESSAGE = '''node_down foo-BETA testhost.localhost:9100 node resolved
 
-Instance testhost.localhost:9100 down
-testhost.localhost:9100 of job node has been down for more than 5 minutes.
+description: testhost.localhost:9100 of job node has been down for more than 5 minutes.
+summary: Instance testhost.localhost:9100 down
 
 Prometheus: https://monitoring.promehteus.localhost/graph#%5B%7B%22expr%22%3A%22up%20%3D%3D%200%22%2C%22tab%22%3A0%7D%5D
 Alert Manager: https://am.promehteus.localhost'''
