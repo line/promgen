02f35718c6f6c3b18851b94e232031738629684e
Paul Traylor <paul.traylor@linecorp.com>
2016-12-28 14:50:17 +0900

    Fix send count and add debug logging

--- promgen/sender/__init__.py
+++ promgen/sender/__init__.py
@@ -7,16 +7,22 @@ logger = logging.getLogger(__name__)
 
 class SenderBase(object):
     def send(self, data):
+        sent = 0
         for alert in data['alerts']:
             if 'project' in alert['labels']:
-                sent = 0
+                logger.debug('Checking for projects')
                 for project in Project.objects.filter(name=alert['labels']['project']):
+                    logger.debug('Checking %s', project)
                     for sender in project.sender.all():
+                        logger.debug('Sending to %s', sender)
                         if self._send(sender.value, alert, data):
                             sent += 1
             if 'service' in alert['labels']:
+                logger.debug('Checking for service')
                 for service in Service.objects.filter(name=alert['labels']['service']):
+                    logger.debug('Checking %s', service)
                     for sender in service.sender.all():
+                        logger.debug('Sending to %s', sender)
                         if self._send(sender.value, alert, data):
                             sent += 1
         if sent == 0:
