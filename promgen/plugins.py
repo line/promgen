a7c9a89cda95f9b9d3d471f63d4bee9f972f02fa
Paul Traylor <paul.traylor@linecorp.com>
2016-12-28 17:25:14 +0900

    Plugin app loader changes from celery branch

--- promgen/plugins.py
+++ promgen/plugins.py
@@ -11,3 +11,5 @@ def remotes():
 
 def senders():
     return working_set.iter_entry_points('promgen.sender')
+
+apps = [entry.module_name for entry in working_set.iter_entry_points('promgen.apps')]
