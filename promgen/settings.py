6c387468e6f985d333e86f4417071f8f81f7193e
Paul Traylor <paul.traylor@linecorp.com>
2017-01-11 12:07:46 +0900

    Documentation for dynamic apps

--- promgen/settings.py
+++ promgen/settings.py
@@ -16,7 +16,7 @@ import dj_database_url
 import envdir
 import yaml
 
-from promgen.plugins import apps
+from promgen.plugins import apps_from_setuptools
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.staticfiles',
     'promgen',
-] + apps
+] + apps_from_setuptools
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
