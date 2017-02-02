e945a62de833f7fc95362e0f9a022f304272a306
Paul Traylor <paul.traylor@linecorp.com>
2017-02-02 11:17:06 +0900

    Use .replaceWith() to replace entire panel to make styling easier

--- promgen/static/js/promgen.js
+++ promgen/static/js/promgen.js
@@ -2,7 +2,7 @@ $( document ).ready(function() {
   $.ajax({url: "/ajax/alert",}).done(function( data ) {
     for (var key in data) {
       console.log("Writing " + key)
-      $("#" + key).html(data[key]).show()
+      $("#" + key).replaceWith(data[key])
   }
   });
 });
