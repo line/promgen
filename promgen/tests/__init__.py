8196c59a0486aae650dabe1ac0746246b9c5da4e
Paul Traylor <paul.traylor@linecorp.com>
2017-04-05 15:19:50 +0900

    Initial replace test

--- promgen/tests/__init__.py
+++ promgen/tests/__init__.py
@@ -2,11 +2,15 @@ import json
 import os
 import yaml
 
+
 with open(os.path.join(os.path.dirname(__file__), 'examples', 'alertmanager.json')) as fp:
     TEST_ALERT = json.load(fp)
 
 with open(os.path.join(os.path.dirname(__file__), 'examples', 'import.json')) as fp:
     TEST_IMPORT = json.load(fp)
+
+with open(os.path.join(os.path.dirname(__file__), 'examples', 'replace.json')) as fp:
+    TEST_REPLACE = json.load(fp)
 
 with open(os.path.join(os.path.dirname(__file__), 'examples', 'settings.yaml')) as fp:
     TEST_SETTINGS = yaml.load(fp)
