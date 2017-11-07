fbd0214a52f1cf6817bdbdd2125182aad26ee6f6
paul.traylor <paul.traylor@linecorp.com>
2017-11-07 12:18:38 +0900

    User notifier (#180)
    * Support for 'user' notifications
    
    * Add a 'user' notification type which takes a username as a target and
      sends notifications with a context_type of User and a id of that user
    * Add a Profile page for users to manage their notifications and
      subscriptions
    * Add a 'subscribe' link to Service and Project pages
    
    * Cleanup naming around plugin/driver
    
    * From the module perspective we use the term 'plugin'
    * From the class/instance perspective we use the term 'driver'

---
+++ promgen/notification/user.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2017 LINE Corporation
+# These sources are released under the terms of the MIT license: see LICENSE
+
+import logging
+
+from django import forms
+from django.contrib.auth.models import User
+
+from promgen import models
+from promgen.notification import NotificationBase
+
+logger = logging.getLogger(__name__)
+
+
+def _choices():
+    for user in User.objects.filter(is_active=True).order_by('username'):
+        if user.first_name:
+            yield (user.username, '{user.username} ({user.first_name} {user.last_name})'.format(user=user))
+        elif user.email:
+            yield (user.username, '{user.username} ({user.email})'.format(user=user))
+        else:
+            yield (user.username, user.username)
+
+
+class FormUser(forms.Form):
+    value = forms.ChoiceField(
+        required=True,
+        label='Username',
+        choices=_choices
+    )
+
+
+class NotificationUser(NotificationBase):
+    '''
+    Send notification to specific user
+    '''
+
+    form = FormUser
+
+    def _send(self, address, data):
+        user = User.objects.get(username=address)
+        for sender in models.Sender.filter(obj=user):
+            try:
+                sender.driver._send(sender.value, data)
+            except:
+                logger.exception('Error sending with %s', sender)
+        return True
