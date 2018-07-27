c02893e8621f85df1bf6065bf321fd1c04a513d6
Seoester <seoester@googlemail.com>
2018-07-27 16:58:29 +0200

    Docker entrypoint: Added wrapper to wait for db
     * Integrated with docker-compose file

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -16,6 +16,17 @@ web)
   shift
   set -- gunicorn "promgen.wsgi:application" "$@"
   ;;
+wait-runserver)
+  # Shortcut to wait for db and start the Promgen web app in development mode (django runserver)
+  until promgen check 2>/dev/null
+  do
+    echo "Waiting for database to startup"
+    sleep 3
+  done
+
+  shift
+  set -- promgen runserver "$@"
+  ;;
 bootstrap|createsuperuser|migrate|shell|test|import|queuecheck|rbimport|register|rules|targets|urls)
   # Shortcuts for some commonly used django commands
   set -- promgen "$@"
