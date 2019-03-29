589350733c8780b5608e55d9b88a8fa62546b416
Paul Traylor <paul.traylor@linecorp.com>
2019-03-29 16:24:45 +0900

    Use django-filters to improve API filtering

---
+++ promgen/filters.py
@@ -0,0 +1,28 @@
+import django_filters
+
+
+class ShardFilter(django_filters.rest_framework.FilterSet):
+    name = django_filters.CharFilter(field_name="name", lookup_expr="contains")
+
+
+class ServiceFilter(django_filters.rest_framework.FilterSet):
+    name = django_filters.CharFilter(field_name="name", lookup_expr="contains")
+    shard = django_filters.CharFilter(field_name="shard__name", lookup_expr="contains")
+
+
+class ProjectFilter(django_filters.rest_framework.FilterSet):
+    name = django_filters.CharFilter(field_name="name", lookup_expr="contains")
+    service = django_filters.CharFilter(
+        field_name="service__name", lookup_expr="contains"
+    )
+    shard = django_filters.CharFilter(
+        field_name="service__shard__name", lookup_expr="contains"
+    )
+
+
+class RuleFilter(django_filters.rest_framework.FilterSet):
+    name = django_filters.CharFilter(field_name="name", lookup_expr="contains")
+    parent = django_filters.CharFilter(
+        field_name="parent__name", lookup_expr="contains"
+    )
+    enabled = django_filters.BooleanFilter(field_name="enabled")
