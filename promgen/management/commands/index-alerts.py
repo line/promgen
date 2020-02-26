9a1307e06f0a3a96bb478cf26af78ef1c17be5ca
Sho Okada <shokada3@gmail.com>
2020-02-26 12:55:51 +0900

    Add a command for indexalert

---
+++ promgen/management/commands/index-alerts.py
@@ -0,0 +1,11 @@
+# Copyright (c) 2020 LINE Corporation
+# These sources are released under the terms of the MIT license: see LICENSE
+
+from django.core.management.base import BaseCommand
+from promgen import models, tasks
+
+
+class Command(BaseCommand):
+    def handle(self, **kargs):
+        for alert in models.Alert.objects.filter(alertlabel__isnull=True):
+            tasks.index_alert.delay(alert.pk)
