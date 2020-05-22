7007e8f1af47a303be68c873d21dc384a37efce3
Paul Traylor <paul.traylor@linecorp.com>
2020-05-22 13:53:21 +0900

    Refactor checks

--- promgen/checks.py
+++ promgen/checks.py
@@ -7,22 +7,24 @@ from django.core import checks
 from promgen import models, util
 
 
-@checks.register(checks.Tags.models)
+# See notes in bootstrap.py
+# @custom.register(checks.Tags.models)
 def sites(app_configs, **kwargs):
     if models.Site.objects.count() == 0:
-        yield checks.Error(
+        yield checks.Warning(
             "Site not configured", hint="Missing django site configuration"
         )
 
     for site in models.Site.objects.filter(
         pk=settings.SITE_ID, domain__in=["example.com"]
     ):
-        yield checks.Error(
+        yield checks.Warning(
             "Site not configured", obj=site, hint="Please update from admin panel"
         )
 
 
-@checks.register(checks.Tags.models)
+# See notes in bootstrap.py
+# @custom.register(checks.Tags.models)
 def shards(**kwargs):
     if models.Shard.objects.filter(enabled=True).count() == 0:
         yield checks.Warning("Missing shards", hint="Ensure some shards are enabled")
