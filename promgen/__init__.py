0b73046bd602da105124369ffdb40843afcca8df
Paul Traylor <paul.traylor@linecorp.com>
2023-09-25 14:23:43 +0900

    Remove envdir
    Now that we're deploying Promgen more often with a docker container, it
    is better to just rely on django-environ and remove the old dependency
    on envdir.

--- promgen/__init__.py
+++ promgen/__init__.py
@@ -16,27 +16,3 @@ PROMGEN_CONFIG_DIR = pathlib.Path(
 PROMGEN_CONFIG_FILE = pathlib.Path(
     os.environ.get("PROMGEN_CONFIG", str(PROMGEN_CONFIG_DIR / "promgen.yml"))
 )
-
-# In the future we want to remove the dependency on envdir, but this will
-# require some cleanup and documentation changes, so for now we will just
-# ensure that it can't fail here if envdir is not already installed
-if PROMGEN_CONFIG_DIR.exists():
-    try:
-        import envdir
-    except ImportError:
-        pass
-    else:
-        envdir.open(PROMGEN_CONFIG_DIR)
-
-
-if "SECRET_KEY" not in os.environ:
-    try:
-        from django.utils.crypto import get_random_string
-        import warnings
-
-        warnings.warn("Unset SECRET_KEY setting to random for now")
-        os.environ["SECRET_KEY"] = get_random_string(
-            50, "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)"
-        )
-    except ImportError:
-        pass
