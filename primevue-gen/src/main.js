b4107bc6f1be270de086044ea665420d09feb63a
Vicente Olivert Riera <vincent.olivert.riera@linecorp.com>
2023-11-22 22:21:43 +0900

    Vue: Add support for Pinia
    This will allow us to define stores using Pinia, which is the new
    standard for Vue stores replacing Vuex.
    So far we just import the library and make the Vue app use Pinia.
    
    The idea is to define the stores in separate JS files living inside a
    stores directory, which will be more organised than our current state
    where all the stores are defined in a single JS file among other things.

--- primevue-gen/src/main.js
+++ primevue-gen/src/main.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
+
+import { createPinia } from "pinia";
+
 import PrimeVue from "primevue/config";
 import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
 
 window.createPrimeVueApp = function (opt) {
   const app = Vue.createApp(opt);
+
+  app.use(createPinia());
   app.use(PrimeVue);
 
   return app;
