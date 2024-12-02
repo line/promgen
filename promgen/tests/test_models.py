20cb1f1f171916ff97ab1bb470af64ad5e7cfb94
hoang <pham.ngoc.hoang@linecorp.com>
2024-12-02 16:23:32 +0700

    Custom the save method of the Rule model to handle the annotation "rule"
    When creating or updating a rule on Promgen, the corresponding rule data on Prometheus will be given
    an annotation rule that is the link to that rule on Promgen. However, the annotation rule on Promgen
    always has the URI "/rule/0" because it has not been intentionally set up.
    
    To ensure data is synced between the two applications, we customize the save method of the Rule
    model.

--- promgen/tests/test_models.py
+++ promgen/tests/test_models.py
@@ -1,10 +1,12 @@
 # Copyright (c) 2020 LINE Corporation
 # These sources are released under the terms of the MIT license: see LICENSE
+from unittest import mock
 
-
+from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
 
 from promgen import models, validators
+from promgen.shortcuts import resolve_domain
 from promgen.tests import PromgenTest
 
 
@@ -34,3 +36,36 @@ class ModelTest(PromgenTest):
             validators.metricname(
                 "[[this.$el.ownerDocument.defaultView.alert(1337)]]",
             )
+
+    @mock.patch("django.dispatch.dispatcher.Signal.send")
+    def test_rule_annotation(self, mock_post):
+        # Check if annotation["rule"] is automatically set to be {domain}/rule/{id} when creating a
+        # new rule
+        rule = models.Rule(
+            name="example-rule",
+            content_type=ContentType.objects.get_for_model(models.Site),
+            object_id=1,
+            clause="up==1",
+            duration="1s"
+        )
+        rule.save()
+        self.assertEqual(resolve_domain("rule-detail", rule.pk), rule.annotations["rule"])
+
+        # Check if annotation["rule"] is automatically set to be {domain}/rule/{id} when updating an
+        # existed rule
+        rule.name = "another-example-rule"
+        rule.annotations["rule"] = "another-annotation-value"
+        rule.save()
+        self.assertEqual(resolve_domain("rule-detail", rule.pk), rule.annotations["rule"])
+
+        # Check if annotation["rule"] is still set to be {domain}/rule/{id} when trying to remove
+        # annotation["rule"]
+        rule.annotations["rule"] = None
+        rule.save()
+        self.assertEqual(resolve_domain("rule-detail", rule.pk), rule.annotations["rule"])
+
+        # Check if annotation["rule"] of new rule is automatically set to be {domain}/rule/{id}
+        # when cloning an existed rule
+        new_rule = rule.copy_to(content_type="service", object_id=2)
+        self.assertEqual(resolve_domain("rule-detail", rule.pk), rule.annotations["rule"])
+        self.assertEqual(resolve_domain("rule-detail", new_rule.pk), new_rule.annotations["rule"])
