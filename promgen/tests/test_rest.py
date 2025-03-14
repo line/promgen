b727dd139113e1a36726c4b673d1448ad96cbcbf
hoang <pham.ngoc.hoang@linecorp.com>
2025-03-14 12:22:21 +0700

    Add v2 APIs for User
     We have added several APIs for retrieving User data.
    - List User
    - Get the User data based on the current Token

--- promgen/tests/test_rest.py
+++ promgen/tests/test_rest.py
@@ -4,6 +4,7 @@
 
 from django.test import override_settings
 from django.urls import reverse
+from rest_framework.authtoken.models import Token
 
 from promgen import models, rest, tests
 
@@ -60,3 +61,55 @@ class RestAPITest(tests.PromgenTest):
         # Check retrieving the farm whose "id" is "1", including the list of hosts.
         response = self.client.get(reverse("api:farm-detail", args=[1]))
         self.assertEqual(response.json(), expected)
+
+    @override_settings(PROMGEN=tests.SETTINGS)
+    def test_rest_user(self):
+        # Check retrieving all users
+        expected = tests.Data("examples", "rest.user.default.json").json()
+        response = self.client.get(reverse("api-v2:user-list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), expected)
+
+        # Check retrieving paginated users
+        expected = tests.Data("examples", "rest.user.paginated.json").json()
+        response = self.client.get(reverse("api-v2:user-list"), {"page_number": 1, "page_size": 1})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), expected)
+
+        # Check retrieving users whose "username" contains "demo"
+        expected = tests.Data("examples", "rest.user.filter_by_username.json").json()
+        response = self.client.get(reverse("api-v2:user-list"), {"username": "demo"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), expected)
+
+        # Check retrieving users with a non-existent "username" returns an empty list
+        response = self.client.get(reverse("api-v2:user-list"), {"username": "non-existent"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["count"], 0)
+
+        # Check retrieving users whose "email" contains "demo@example"
+        expected = tests.Data("examples", "rest.user.filter_by_email.json").json()
+        response = self.client.get(reverse("api-v2:user-list"), {"email": "demo@example"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), expected)
+
+        # Check retrieving users with a non-existent "email" returns an empty list
+        response = self.client.get(reverse("api-v2:user-list"), {"email": "non-existent"})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["count"], 0)
+
+        # Check retrieving the current user without token returns 401 Unauthorized
+        response = self.client.get(reverse("api-v2:user-get-current-user"))
+        self.assertEqual(response.status_code, 401)
+
+        # Check retrieving the current user with token returns the expected user
+        expected = tests.Data("examples", "rest.user.get_current_user.json").json()
+        token = Token.objects.filter(user__username="demo").first().key
+        response = self.client.get(
+            reverse("api-v2:user-get-current-user"), HTTP_AUTHORIZATION=f"Token {token}"
+        )
+        response_json = response.json()
+        # date_joined is not a fixed value when running tests, so we need to skip it
+        response_json["date_joined"] = None
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_json, expected)
