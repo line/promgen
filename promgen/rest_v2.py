c7992ba1631ae785f2cc916764d12a0746a6f177
hoang <pham.ngoc.hoang@linecorp.com>
2025-03-14 12:24:07 +0700

    Add new API for retrieve Audit Logs
    We have added a new API for retrieving and filtering Audit Log data.

--- promgen/rest_v2.py
+++ promgen/rest_v2.py
@@ -1,16 +1,18 @@
 # Copyright (c) 2026 LINE Corporation
 # These sources are released under the terms of the MIT license: see LICENSE
 
+from django.contrib.contenttypes.models import ContentType
+from django.db.models import Q
 from django.urls import re_path
-from drf_spectacular.utils import (
-    extend_schema,
-)
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from drf_spectacular.views import SpectacularAPIView
-from rest_framework import pagination, routers
+from rest_framework import mixins, pagination, routers, viewsets
 from rest_framework.authtoken.models import Token
 from rest_framework.renderers import TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+
+from promgen import filters, models, permissions, serializers
 
 
 class SpectacularRapiDocView(APIView):
@@ -62,4 +64,48 @@ class PromgenPagination(pagination.PageNumberPagination):
         self.page_query_description = self.page_query_description + " Starts from 1."
         self.page_size_query_description = self.page_size_query_description + str.format(
             " Defaults to {}.", self.page_size
+        )
+
+
+@extend_schema_view(
+    list=extend_schema(summary="List Audit Logs", description="Retrieve a list of all audit logs."),
+)
+@extend_schema(tags=["Log"])
+class AuditViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    queryset = models.Audit.objects.all().order_by("-created")
+    filterset_class = filters.AuditFilter
+    serializer_class = serializers.AuditSerializer
+    lookup_value_regex = "[^/]+"
+    lookup_field = "id"
+    pagination_class = PromgenPagination
+    permission_classes = [permissions.PromgenGuardianRestPermission]
+
+    def get_queryset(self):
+        if self.request.user.is_superuser or self.action != "list":
+            return self.queryset
+        services = permissions.get_accessible_services_for_user(self.request.user)
+        projects = permissions.get_accessible_projects_for_user(self.request.user)
+        groups = permissions.get_accessible_groups_for_user(self.request.user)
+        service_ct = ContentType.objects.get_for_model(models.Service)
+        project_ct = ContentType.objects.get_for_model(models.Project)
+        group_ct = ContentType.objects.get_for_model(models.Group)
+        return self.queryset.filter(
+            Q(
+                content_type__model="service",
+                content_type__app_label="promgen",
+                object_id__in=services,
+            )
+            | Q(
+                content_type__model="project",
+                content_type__app_label="promgen",
+                object_id__in=projects,
+            )
+            | Q(
+                content_type__model="group",
+                content_type__app_label="auth",
+                object_id__in=groups,
+            )
+            | Q(parent_content_type_id=service_ct.id, parent_object_id__in=services)
+            | Q(parent_content_type_id=project_ct.id, parent_object_id__in=projects)
+            | Q(parent_content_type_id=group_ct.id, parent_object_id__in=groups)
         )
