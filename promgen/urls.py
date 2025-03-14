bc0fe88c3fb61e8d5adae71b662a0194e1a89490
hoang <pham.ngoc.hoang@linecorp.com>
2025-03-14 12:45:57 +0700

    Add v2 APIs for Shard
    We have added several APIs for retrieving Shard data and the list of projects for each shard.

--- promgen/urls.py
+++ promgen/urls.py
@@ -42,6 +42,7 @@ v2_router.register("exporters", rest_v2.ExporterViewSet)
 v2_router.register("urls", rest_v2.URLViewSet)
 v2_router.register("projects", rest_v2.ProjectViewSet)
 v2_router.register("services", rest_v2.ServiceViewSet)
+v2_router.register("shards", rest_v2.ShardViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
