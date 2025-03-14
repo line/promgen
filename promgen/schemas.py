a4db026c739d78f7ef2a5e72574e3d3c897fb83c
hoang <pham.ngoc.hoang@linecorp.com>
2025-03-14 11:44:22 +0700

    Add drf-spectacular
    We plan to add version v2 to Promgen's API. We have chosen drf-spectacular as
    the OpenAPI 3 Schema Generation for these new APIs.
    We also use Rapidoc for the OpenAPI Spec Documentation.

---
+++ promgen/schemas.py
@@ -0,0 +1,40 @@
+from drf_spectacular.openapi import AutoSchema
+from drf_spectacular.plumbing import get_relative_url, set_query_parameters
+from drf_spectacular.settings import spectacular_settings
+from drf_spectacular.utils import extend_schema
+from drf_spectacular.views import AUTHENTICATION_CLASSES
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework.views import APIView
+
+
+class CustomSchema(AutoSchema):
+    def is_excluded(self):
+        return not self.path.startswith("/rest/v2/") or self.path.startswith("/rest/v2/schema/")
+
+
+class SpectacularRapiDocView(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    permission_classes = spectacular_settings.SERVE_PERMISSIONS
+    authentication_classes = AUTHENTICATION_CLASSES
+    url_name = "schema"
+    url = None
+    template_name = "drf_spectacular/rapidoc.html"
+    title = spectacular_settings.TITLE
+
+    @extend_schema(exclude=True)
+    def get(self, request, *args, **kwargs):
+        return Response(
+            data={
+                "title": self.title,
+                "schema_url": self._get_schema_url(request),
+            },
+            template_name=self.template_name,
+        )
+
+    def _get_schema_url(self, request):
+        schema_url = self.url or get_relative_url(reverse(self.url_name, request=request))
+        return set_query_parameters(
+            url=schema_url, lang=request.GET.get("lang"), version=request.GET.get("version")
+        )
