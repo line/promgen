ac87a4ac5244d0dca0cd3c3d56c6a39dccde8d81
hoang <pham.ngoc.hoang@linecorp.com>
2025-06-16 16:41:34 +0700

    Add validation when changing the owner
    Currently, the owner of an object will always have the Admin role on that object.
    Therefore, to emphasize the importance of the owner, the owner of an object can only
    be changed if the user making the change is the current owner of that object.

--- promgen/views.py
+++ promgen/views.py
@@ -526,6 +526,11 @@ class FarmUpdate(LoginRequiredMixin, UpdateView):
 
     def form_valid(self, form):
         if "owner" in form.changed_data:
+            if not (
+                self.request.user.is_superuser or self.request.user.id == form.initial["owner"]
+            ):
+                form.add_error("owner", _("You do not have permission to change the owner."))
+                return self.form_invalid(form)
             assign_perm("farm_admin", form.cleaned_data["owner"], form.instance)
         farm, created = models.Farm.objects.update_or_create(
             id=self.kwargs["pk"],
@@ -819,6 +824,11 @@ class ProjectUpdate(LoginRequiredMixin, UpdateView):
 
     def form_valid(self, form):
         if "owner" in form.changed_data:
+            if not (
+                self.request.user.is_superuser or self.request.user.id == form.initial["owner"]
+            ):
+                form.add_error("owner", _("You do not have permission to change the owner."))
+                return self.form_invalid(form)
             assign_perm("project_admin", form.cleaned_data["owner"], form.instance)
         return super().form_valid(form)
 
@@ -830,6 +840,11 @@ class ServiceUpdate(LoginRequiredMixin, UpdateView):
 
     def form_valid(self, form):
         if "owner" in form.changed_data:
+            if not (
+                self.request.user.is_superuser or self.request.user.id == form.initial["owner"]
+            ):
+                form.add_error("owner", _("You do not have permission to change the owner."))
+                return self.form_invalid(form)
             assign_perm("service_admin", form.cleaned_data["owner"], form.instance)
         return super().form_valid(form)
 
