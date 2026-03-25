296ff09a052fecdee0fecf0a9388011f4a84d4ba
hoang <pham.ngoc.hoang@linecorp.com>
2026-03-25 09:57:19 +0700

    Add Edit Silence feature
    We have added a new feature that allows users to easily edit an existing Silence instead of
    expiring it and creating a new one. Now, users can select a Silence from the list to edit fields
    such as matchers, startsAt, endsAt, duration and comment.

--- promgen/static/js/promgen.js
+++ promgen/static/js/promgen.js
@@ -156,6 +156,16 @@ function activateTabFromHash() {
   }
 }
 
+function UTCToFormattedLocalDateTime(utcString) {
+  const date = new Date(utcString);
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  return `${year}-${month}-${day}T${hours}:${minutes}`;
+}
+
 $(document).ready(function() {
   $('[data-toggle="popover"]').popover();
   $('[data-toggle="tooltip"]').tooltip();
