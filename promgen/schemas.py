ac78b6462448588d1a26a4cb33d004320a4e1935
hoang <pham.ngoc.hoang@linecorp.com>
2026-05-25 22:58:49 +0700

    Remove the trailing slash in v2 APIs
    By default, Django REST Framework initializes endpoints with a trailing slash. Additionally, if
    the router cannot resolve an endpoint without a trailing slash, Django's CommonMiddleware
    automatically responds with a redirect to the same endpoint with a trailing slash. As a result,
    Promgen's APIs currently support both with and without a trailing slash when accessed from a
    browser or a client capable of automatic redirection.
    
    However, a RESTful API should not include a trailing slash because we are accessing resources,
    not directories. Moreover, automatic redirection is not appropriate in designing RESTful API.
    Therefore, we have decided to disable the trailing slash added by Django REST Framework. A 404
    Not Found error will be returned when accessing an endpoint with a trailing slash.

--- promgen/schemas.py
+++ promgen/schemas.py
@@ -3,4 +3,4 @@ from drf_spectacular.openapi import AutoSchema
 
 class CustomSchema(AutoSchema):
     def is_excluded(self):
-        return not self.path.startswith("/rest/v2/") or self.path.startswith("/rest/v2/schema/")
+        return not self.path.startswith("/rest/v2/") or self.path == "/rest/v2/schema"
